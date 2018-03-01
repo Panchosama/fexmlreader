44104f115902da0972a895ed3dd202185112b349
Francisco Figueroa <panchosama@gmail.com>
2018-03-01 10:51:21 -0300

    Agregar archivos de proyecto.

---
+++ fexmlreader/Form1.Designer.vb
@@ -0,0 +1,324 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblArchivo = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.FacturaDataSet = New System.Data.DataSet()
+        Me.LeerXMLButton = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtFolio = New System.Windows.Forms.TextBox()
+        Me.txtFecha = New System.Windows.Forms.TextBox()
+        Me.txtRazonSocial = New System.Windows.Forms.TextBox()
+        Me.txtRut = New System.Windows.Forms.TextBox()
+        Me.txtGiro = New System.Windows.Forms.TextBox()
+        Me.txtNeto = New System.Windows.Forms.TextBox()
+        Me.txtIva = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.btnImprimir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtExento = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FacturaDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblArchivo
+        '
+        Me.lblArchivo.AutoSize = True
+        Me.lblArchivo.Location = New System.Drawing.Point(148, 17)
+        Me.lblArchivo.Name = "lblArchivo"
+        Me.lblArchivo.Size = New System.Drawing.Size(0, 13)
+        Me.lblArchivo.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Folio Factura"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(328, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Fecha de Emisión"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Razón Social"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 89)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(30, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "RUT"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(163, 91)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(26, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Giro"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 274)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(57, 13)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Valor Neto"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(163, 273)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(24, 13)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "IVA"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(444, 273)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(31, 13)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Total"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 113)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(556, 141)
+        Me.DataGridView1.TabIndex = 13
+        '
+        'FacturaDataSet
+        '
+        Me.FacturaDataSet.DataSetName = "FacturaDataSet"
+        '
+        'LeerXMLButton
+        '
+        Me.LeerXMLButton.Location = New System.Drawing.Point(12, 12)
+        Me.LeerXMLButton.Name = "LeerXMLButton"
+        Me.LeerXMLButton.Size = New System.Drawing.Size(84, 23)
+        Me.LeerXMLButton.TabIndex = 14
+        Me.LeerXMLButton.Text = "Abrir XML"
+        Me.LeerXMLButton.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(102, 17)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(46, 13)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "Archivo:"
+        '
+        'txtFolio
+        '
+        Me.txtFolio.Enabled = False
+        Me.txtFolio.Location = New System.Drawing.Point(85, 47)
+        Me.txtFolio.Name = "txtFolio"
+        Me.txtFolio.Size = New System.Drawing.Size(140, 20)
+        Me.txtFolio.TabIndex = 18
+        '
+        'txtFecha
+        '
+        Me.txtFecha.Enabled = False
+        Me.txtFecha.Location = New System.Drawing.Point(425, 47)
+        Me.txtFecha.Name = "txtFecha"
+        Me.txtFecha.Size = New System.Drawing.Size(139, 20)
+        Me.txtFecha.TabIndex = 19
+        '
+        'txtRazonSocial
+        '
+        Me.txtRazonSocial.Enabled = False
+        Me.txtRazonSocial.Location = New System.Drawing.Point(85, 68)
+        Me.txtRazonSocial.Name = "txtRazonSocial"
+        Me.txtRazonSocial.Size = New System.Drawing.Size(479, 20)
+        Me.txtRazonSocial.TabIndex = 20
+        '
+        'txtRut
+        '
+        Me.txtRut.Enabled = False
+        Me.txtRut.Location = New System.Drawing.Point(53, 88)
+        Me.txtRut.Name = "txtRut"
+        Me.txtRut.Size = New System.Drawing.Size(104, 20)
+        Me.txtRut.TabIndex = 21
+        '
+        'txtGiro
+        '
+        Me.txtGiro.Enabled = False
+        Me.txtGiro.Location = New System.Drawing.Point(195, 89)
+        Me.txtGiro.Name = "txtGiro"
+        Me.txtGiro.Size = New System.Drawing.Size(369, 20)
+        Me.txtGiro.TabIndex = 22
+        '
+        'txtNeto
+        '
+        Me.txtNeto.Enabled = False
+        Me.txtNeto.Location = New System.Drawing.Point(70, 270)
+        Me.txtNeto.Name = "txtNeto"
+        Me.txtNeto.Size = New System.Drawing.Size(87, 20)
+        Me.txtNeto.TabIndex = 23
+        '
+        'txtIva
+        '
+        Me.txtIva.Enabled = False
+        Me.txtIva.Location = New System.Drawing.Point(184, 271)
+        Me.txtIva.Name = "txtIva"
+        Me.txtIva.Size = New System.Drawing.Size(87, 20)
+        Me.txtIva.TabIndex = 24
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Enabled = False
+        Me.txtTotal.Location = New System.Drawing.Point(481, 271)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(87, 20)
+        Me.txtTotal.TabIndex = 25
+        '
+        'btnImprimir
+        '
+        Me.btnImprimir.Location = New System.Drawing.Point(456, 12)
+        Me.btnImprimir.Name = "btnImprimir"
+        Me.btnImprimir.Size = New System.Drawing.Size(112, 23)
+        Me.btnImprimir.TabIndex = 26
+        Me.btnImprimir.Text = "Imprimir"
+        Me.btnImprimir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(277, 274)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 27
+        Me.Label1.Text = "Valor Exento"
+        '
+        'txtExento
+        '
+        Me.txtExento.Enabled = False
+        Me.txtExento.Location = New System.Drawing.Point(346, 271)
+        Me.txtExento.Name = "txtExento"
+        Me.txtExento.Size = New System.Drawing.Size(87, 20)
+        Me.txtExento.TabIndex = 28
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(580, 313)
+        Me.Controls.Add(Me.txtExento)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnImprimir)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtIva)
+        Me.Controls.Add(Me.txtNeto)
+        Me.Controls.Add(Me.txtGiro)
+        Me.Controls.Add(Me.txtRut)
+        Me.Controls.Add(Me.txtRazonSocial)
+        Me.Controls.Add(Me.txtFecha)
+        Me.Controls.Add(Me.txtFolio)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.LeerXMLButton)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblArchivo)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "feXMLreader"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FacturaDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblArchivo As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents FacturaDataSet As System.Data.DataSet
+    Friend WithEvents LeerXMLButton As System.Windows.Forms.Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtFolio As TextBox
+    Friend WithEvents txtFecha As TextBox
+    Friend WithEvents txtRazonSocial As TextBox
+    Friend WithEvents txtRut As TextBox
+    Friend WithEvents txtGiro As TextBox
+    Friend WithEvents txtNeto As TextBox
+    Friend WithEvents txtIva As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents btnImprimir As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtExento As TextBox
+End Class
---
+++ fexmlreader/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    'Dim archivo As String
+
+    Private Function AbrirArchivo()
+        Dim ofd = OpenFileDialog1
+        ofd.Filter = "Archivo XML (*.xml)|*.xml"
+        If ofd.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Return ofd.FileName
+        End If
+    End Function
+    Sub CargaEmisor(archivo As String)
+        Dim xmld As Xml.XmlDocument
+        Dim xmlist As Xml.XmlNodeList
+        Dim xmlistdoc As Xml.XmlNodeList
+        Dim xmlisttot As Xml.XmlNodeList
+        Dim xmln As Xml.XmlNode
+        xmld = New Xml.XmlDocument()
+        xmld.Load(archivo)
+        lblArchivo.Text = System.IO.Path.GetFileName(archivo)
+        xmlist = xmld.GetElementsByTagName("Emisor")
+        For Each xmln In xmlist
+            Dim rutEmisor = xmln.ChildNodes.Item(0).InnerText
+            Dim rznSocial = xmln.ChildNodes.Item(1).InnerText
+            Dim giroEmis = xmln.ChildNodes.Item(2).InnerText
+
+            txtRut.Text = rutEmisor
+            txtRazonSocial.Text = rznSocial
+            txtGiro.Text = giroEmis
+        Next
+        xmlistdoc = xmld.GetElementsByTagName("IdDoc")
+        For Each xmln In xmlistdoc
+            Dim folio = xmln.ChildNodes.Item(1).InnerText
+            Dim fechaEm = xmln.ChildNodes.Item(2).InnerText
+
+            txtFolio.Text = folio
+            txtFecha.Text = fechaEm
+        Next
+        xmlisttot = xmld.GetElementsByTagName("Totales")
+        For Each xmln In xmlisttot
+            Dim mntNeto = xmln.ChildNodes.Item(0).InnerText
+            Dim iva = xmln.ChildNodes.Item(2).InnerText
+            Dim total = xmln.ChildNodes.Item(3).InnerText
+
+            txtNeto.Text = mntNeto
+            txtIva.Text = iva
+            txtTotal.Text = total
+        Next
+    End Sub
+    Private Sub LeerXMLButton_Click(sender As System.Object, e As EventArgs) Handles LeerXMLButton.Click
+        'Dim filePath As String = "C:\Users\Informática\Desktop\fe xml\prisa.xml"
+        Try
+            Dim filePath As String = AbrirArchivo()
+            FacturaDataSet.Clear()
+
+            FacturaDataSet.ReadXml(filePath)
+            CargaEmisor(filePath)
+
+        Catch ex As System.NullReferenceException
+            'MessageBox.Show("Error: " + ex.Message)
+            Exit Sub
+
+        End Try
+
+
+        DataGridView1.DataSource = FacturaDataSet
+        DataGridView1.DataMember = "Detalle"
+    End Sub
+
+    Private Sub MostrarSchemaButton_Click(sender As System.Object, e As System.EventArgs)
+        Dim swXML As New System.IO.StringWriter()
+        FacturaDataSet.WriteXmlSchema(swXML)
+        txtRut.Text = swXML.ToString
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
